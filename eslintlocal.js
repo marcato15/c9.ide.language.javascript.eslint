39241b267e77ee2324aad6ca0e337c0730a5de1a
Marc Tanis <marcato15@gmail.com>
2017-12-09 19:44:13 -0600

    Update to eslintlocal

---
+++ eslintlocal.js
@@ -0,0 +1,31 @@
+/**
+ * Cloud9 Language Foundation
+ *
+ * @copyright 2013, Ajax.org B.V.
+ */
+define(function(require, exports, module) {
+    main.consumes = ["language","Plugin"];
+    main.provides = ["eslintlocal"];
+
+    function main(options, imports, register) {
+        var Plugin = imports.Plugin;
+        var language = imports.language;
+
+        var plugin = new Plugin("eslintlocal", main.consumes);
+
+        plugin.on("load", function () {
+            language.unregisterLanguageHandler("plugins/c9.ide.language.javascript.eslint/worker/eslint_worker");
+            language.registerLanguageHandler("plugins/c9.ide.language.javascript.eslint-local/worker/eslintlocal_worker");
+        });
+
+        plugin.on("unload", function () {
+            language.unregisterLanguageHandler("plugins/c9.ide.language.javascript.eslint-local/worker/eslintlocal_worker");
+            language.registerLanguageHandler("plugins/c9.ide.language.javascript.eslint/worker/eslint_worker");
+        });
+
+        
+        register(null, {eslintlocal: plugin});
+    }
+    
+    return main;
+});
